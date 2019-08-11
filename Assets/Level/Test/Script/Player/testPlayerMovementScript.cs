e6be00e6685e55b5052be87019ec86b136416cbe
Yanno693 <ututa@hotmail.fr>
2019-08-11 21:51:49 +0200

    Presque fini mouvement et rotation
    Movement and rotation almost finiched

--- Assets/Level/Test/Script/Player/testPlayerMovementScript.cs
+++ Assets/Level/Test/Script/Player/testPlayerMovementScript.cs
@@ -8,31 +8,57 @@ public class testPlayerMovementScript : MonoBehaviour /// Handle basic player mo
     public Camera currentCamera;
     
     // Start is called before the first frame update
-    public float speed;
-    public float rotation;
+    public float movementSpeed;
+    public float rotationSpeed;
+
+    private readonly Vector2 VEC2_FORWARD = new Vector2(0, 1);
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        speed = 10.0f;
-        rotation = 50.0f;
+        movementSpeed = 5.0f;
+        rotationSpeed = 10.0f;
+
+    }
+
+    public static Vector2 Vec2Rotate(Vector2 v, float rad)
+    {
+        return new Vector2(
+            v.x * Mathf.Cos(rad) - v.y * Mathf.Sin(rad),
+            v.x * Mathf.Sin(rad) + v.y * Mathf.Cos(rad)
+        );
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Character Motion (Forward and backward for now)
+        // Character Motion for roaming camera (no rotation of character for now)
 
-        Vector3 forward = new Vector3();
-        forward = transform.position - currentCamera.transform.position;
+        Vector3 forward = transform.position - currentCamera.transform.position;
         forward.y = 0;
-        forward.Normalize();
+        Vector2 directionForward = new Vector2(forward.x, forward.z).normalized;
 
-        characterController.Move(forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
-        //characterController.Move(transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
-        
-        
-        
-        transform.Rotate(0,Input.GetAxis("Horizontal") * rotation * Time.deltaTime,0);
+        Vector2 directionInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        float angleBetween = Vector2.SignedAngle(VEC2_FORWARD, directionForward) * Mathf.Deg2Rad;
+        Vector2 directionRotation = Vec2Rotate(directionInput, angleBetween);
+
+        characterController.Move(new Vector3(directionRotation.x,0,directionRotation.y) * movementSpeed * Time.deltaTime);
+
+        if(directionRotation.magnitude > 0.1) {
+            //transform.rotation = Quaternion.LookRotation(new Vector3(directionRotation.x,0,directionRotation.y), Vector3.up);
+
+            transform.rotation = Quaternion.Lerp(
+                transform.rotation,
+                Quaternion.LookRotation(new Vector3(directionRotation.x, 0, directionRotation.y)),
+                Time.deltaTime * rotationSpeed * ( 
+                    Quaternion.Angle(
+                         transform.rotation,
+                         Quaternion.LookRotation(new Vector3(directionRotation.x, 0, directionRotation.y)
+                        )
+                    ) * Mathf.Deg2Rad
+                )
+            );
+        }
     }
 }
