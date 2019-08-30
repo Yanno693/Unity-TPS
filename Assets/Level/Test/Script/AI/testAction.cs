044a7861f244510522cccc8f0a102313890bc070
Yanno693 <ututa@hotmail.fr>
2019-08-30 18:29:19 +0200

    Test d'arme avec raycast
    Weapon test with raycast

--- Assets/Level/Test/Script/AI/testAction.cs
+++ Assets/Level/Test/Script/AI/testAction.cs
@@ -7,7 +7,7 @@ public class testAction : NPCAction // It's just a test
 {
     public override void Do(NPC npc)
     {
-        npc.transform.Translate(Time.deltaTime,0,0);
+        //npc.transform.Translate(Time.deltaTime,0,0);
 
         //if(npc is SomethingNPC)
     }
--- Assets/Level/Test/Script/Interactable/Weapon/TestWeaponScript.cs
+++ Assets/Level/Test/Script/Interactable/Weapon/TestWeaponScript.cs
@@ -32,6 +32,28 @@ public class TestWeaponScript : Weapon
             Debug.Log("Weapon fired");
             lastFire = 0f;
             currentClipAmmo--;
+
+            if(humanoid is Player)
+            {
+                Player player = (Player)humanoid;
+                Vector3 rayCastStart = player.gameObject.GetComponent<testPlayerMovementScript>().currentCamera.transform.position;
+
+                int layerMask = ~(1 << LayerMask.NameToLayer("Humanoid"));
+                //int layerMask =~ 1;
+
+                RaycastHit hit;
+                if(Physics.Raycast(rayCastStart, player.gameObject.GetComponent<testPlayerMovementScript>().currentCamera.transform.forward, out hit, 100f, layerMask)) // First for camera direction
+                {
+                    
+                    RaycastHit hit2;
+                    Vector3 newCastDirection = (hit.point - player.transform.Find("Nose").gameObject.transform.position).normalized;
+                    if(Physics.Raycast(player.transform.Find("Nose").gameObject.transform.position, newCastDirection, out hit2, 100f))
+                    {
+                        Debug.DrawLine(player.transform.Find("Nose").gameObject.transform.position, hit2.point, Color.magenta, 0.1f);
+                        Debug.Log(hit2.collider.gameObject);
+                    }
+                }
+            }
         }
     }
 
